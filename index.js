ba36da8979c72e6044b78d8168ab486b9f5ed57a
do7be <bomsap700@gmail.com>
2016-01-24 14:53:06 +0900

    support --cached

--- index.js
+++ index.js
@@ -10,19 +10,28 @@ let path    = require('path'),
     program = require('commander');
 
 // set program info
-program.version('0.0.1').usage('[options] [<commit>] [--] [<path>...]').parse(process.argv);
+program
+  .version('0.0.1')
+  .usage('[options] [<commit>] [--] [<path>...]')
+  .option('--cached', 'show diff of staging files')
+  .parse(process.argv);
+
+// judge options
+let options=[];
+if (program.cached) {
+  options.push('--cached');
+}
 
 // init output file
 let realPath = path.dirname(fs.realpathSync(__filename));
-let outputJsText = 'var lineDiffExample="";';
-fs.writeFileSync(`${realPath}/dest/diff.js`, outputJsText);
+outputJs(realPath, '');
 
-var giff = spawn('git', ['diff'].concat(program.args));
+// git diff
+var giff = spawn('git', ['diff'].concat(program.args).concat(options));
 giff.stdout.on('data', function (data) {
   // git diff result encode to base64
   let base64Diff = data.toString('Base64');
-  let outputJsText = 'var lineDiffExample=window.atob("' + base64Diff + '");';
-  fs.writeFileSync(`${realPath}/dest/diff.js`, outputJsText);
+  outputJs(realPath, base64Diff);
 });
 
 giff.stderr.on('data', function (data) {
@@ -34,3 +43,8 @@ giff.on('exit', function (code) {
 
 console.log(`${realPath}/index.html`);
 exec(`which open && open ${realPath}/index.html`);
+
+function outputJs(dirPath, data) {
+  let outputJsText = 'var lineDiffExample=window.atob("' + data + '");';
+  fs.writeFileSync(`${dirPath}/dest/diff.js`, outputJsText);
+}
