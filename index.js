869e2dad1c4e6fbbff26532721ff6bfb7d95419d
do7be <bomsap700@gmail.com>
2016-02-03 11:29:53 +0900

    version up for cdn libraries to local

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let path    = require('path'),
 
 // set program info
 program
-  .version('0.0.4')
+  .version('0.0.5')
   .usage('[options] [<commit>] [--] [<path>...]')
   .option('--cached', 'show diff of staging files')
   .parse(process.argv);
